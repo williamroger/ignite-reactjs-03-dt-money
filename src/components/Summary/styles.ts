40eba862a4374184e11c000003b516c3c35ea204
William Roger <williamroger.frontend@gmail.com>
2024-02-12 21:14:41 -0300

    adicionado componente Summary

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,41 @@
+import styled, { css } from "styled-components";
+
+export const SummaryContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
+`;
+
+interface SummaryCardProps {
+  variant?: 'green',
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background-color: ${({ theme }) => theme["gray-700"]};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${({ theme }) => theme["gray-300"]};
+  }
+
+  strong {
+    display: block;
+    font-size: 2rem;
+    margin-top: 1rem;
+  }  
+
+  ${({ theme, variant }) => variant === 'green' && css`
+    background-color: ${theme['green-700']};
+  `}
+`;
